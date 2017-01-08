5fc9ce35a75fdccd15d9520c78dbd2aca12a233f
Vincent <ktmswzw@gmail.com>
2017-01-08 13:10:27 +0800

    bug before2

--- src/main/java/com/xecoder/Ng2ServerApplication.java
+++ src/main/java/com/xecoder/Ng2ServerApplication.java
@@ -1,9 +1,12 @@
 package com.xecoder;
 
+import com.xecoder.config.MybatisMapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.xecoder.core.mapper", markerInterface = MybatisMapper.class)
 public class Ng2ServerApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(Ng2ServerApplication.class, args);
